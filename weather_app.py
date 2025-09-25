82b13474c3a4a9cd6fc094ac87898bfbb0a5141d
acier34 <eng.lucas.felix@gmail.com>
2025-09-24 21:18:21 -0300

    weather_app.py

--- weather_app.py
+++ weather_app.py
@@ -2,19 +2,7 @@
 """
 Streamlit Weather App
 =====================
-
-This is a basic Streamlit webapp displaying the current temperature, wind speed
-and wind direction as well as the temperature and precipitation forecast for 
-the week ahead at one of 40,000+ cities and towns around the globe. The weather
-forecast is given in terms of the actual timezone of the city of interest.
-Additionally, a map with the location of the requested city is displayed.
-
-- The weather data is from http://open-meteo.com
-- The list with over 40,000 cities around world stems from 
-  https://simplemaps.com/data/world-cities
-
-Enjoy exploring!
-
+(O restante do seu comentário inicial permanece o mesmo)
 """
 
 import streamlit as st
@@ -26,7 +14,7 @@ import plotly.graph_objs as go
 from datetime import datetime
 from datetime import timezone as tmz
 import pytz
-from tzwhere import tzwhere
+from timezonefinder import TimezoneFinder # <-- MUDANÇA 1: Importa a nova biblioteca
 import folium
 from streamlit_folium import folium_static
 
@@ -36,17 +24,21 @@ st.title("How's the weather? :sun_behind_rain_cloud:")
 
 st.subheader("Choose location")
 
-file = "worldcities.csv"
-data = pd.read_csv(file)
+# Carrega o arquivo de forma mais segura para o Streamlit Cloud
+@st.cache_data
+def load_data(file_path):
+    return pd.read_csv(file_path)
+
+data = load_data("worldcities.csv")
 
 
 # Select Country
-country_set = set(data.loc[:,"country"])
+country_set = sorted(list(set(data.loc[:,"country"])))
 country = st.selectbox('Select a country', options=country_set)
 
 country_data = data.loc[data.loc[:,"country"] == country,:]
 
-city_set = country_data.loc[:,"city_ascii"] 
+city_set = sorted(list(country_data.loc[:,"city_ascii"]))
 
 city = st.selectbox('Select a city', options=city_set)
 
@@ -68,7 +60,7 @@ direction = current["winddirection"]
 # Increment added or substracted from degree values for wind direction
 ddeg = 11.25
 
-# Determine wind direction based on received degrees
+# (Toda a sua lógica para determinar a direção do vento permanece a mesma)
 if direction >= (360-ddeg) or direction < (0+ddeg):
     common_dir = "N"
 elif direction >= (337.5-ddeg) and direction < (337.5+ddeg):
@@ -118,12 +110,13 @@ with st.spinner('Loading...'):
     hourly_df.rename(columns = {'temperature_2m':'Temperature °C'}, inplace = True)
     hourly_df.rename(columns = {'precipitation':'Precipitation mm'}, inplace = True)
     
-    tz = tzwhere.tzwhere(forceTZ=True)
-    timezone_str = tz.tzNameAt(lat, lng, forceTZ=True) # Seville coordinates
+    tf = TimezoneFinder() # <-- MUDANÇA 2: Cria o objeto da nova biblioteca
+    timezone_str = tf.timezone_at(lng=lng, lat=lat) # <-- MUDANÇA 3: Usa o novo método
     
+    # Daqui para baixo, o resto do seu código funciona perfeitamente com a nova biblioteca
     timezone_loc = pytz.timezone(timezone_str)
     dt = datetime.now()
-    tzoffset = timezone_loc.utcoffset(dt)#-timedelta(hours=1,minutes=0)
+    tzoffset = timezone_loc.utcoffset(dt)
     
     
     # Create figure with secondary y axis
@@ -147,22 +140,22 @@ with st.spinner('Loading...'):
     
     fig.add_vline(x = time_now, line_color="red", opacity=0.4)
     fig.add_annotation(x = time_now, y=max(hourly_df['Temperature °C'])+5,
-                text = time_now.strftime("%d %b %y, %H:%M"),
-                showarrow=False,
-                yshift=0)
+                 text = time_now.strftime("%d %b %y, %H:%M"),
+                 showarrow=False,
+                 yshift=0)
     
     fig.update_yaxes(range=[min(hourly_df['Temperature °C'])-10,
                             max(hourly_df['Temperature °C'])+10],
-                      title_text="Temperature °C",
-                     secondary_y=False,
-                     showgrid=False,
-                     zeroline=False)
+                     title_text="Temperature °C",
+                    secondary_y=False,
+                    showgrid=False,
+                    zeroline=False)
         
     fig.update_yaxes(range=[min(hourly_df['Precipitation mm'])-2,
                             max(hourly_df['Precipitation mm'])+8], 
-                      title_text="Precipitation (rain/showers/snow) mm",
-                     secondary_y=True,
-                     showgrid=False)
+                     title_text="Precipitation (rain/showers/snow) mm",
+                    secondary_y=True,
+                    showgrid=False)
     
     
     fig.update_layout(legend=dict(
@@ -176,10 +169,9 @@ with st.spinner('Loading...'):
     # center on Liberty Bell, add marker
     m = folium.Map(location=[lat, lng], zoom_start=7)
     folium.Marker([lat, lng], 
-              popup=city+', '+country, 
-              tooltip=city+', '+country).add_to(m)
+                  popup=city+', '+country, 
+                  tooltip=city+', '+country).add_to(m)
     
-    # call to render Folium map in Streamlit
     
     # Make folium map responsive to adapt to smaller display size (
     # e.g., on smartphones and tablets)
@@ -196,12 +188,9 @@ with st.spinner('Loading...'):
     # Display map
     st_data = folium_static(m, height = 370)
     
-       
+        
     # Concluding remarks
     st.write('Weather data source: [http://open-meteo.com](http://open-meteo.com) \n\n'+
              'List of 40,000+ world cities: [https://simplemaps.com/data/world-cities](https://simplemaps.com/data/world-cities) \n\n' +
              'Github repository: [streamlit-weather-app](https://github.com/ndakov/streamlit-weather-app)')
     st.write('Thanks for stopping by. Cheers!')
-
-
-
